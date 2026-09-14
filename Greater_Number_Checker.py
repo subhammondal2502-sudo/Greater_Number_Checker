003a44cc1d6c019a7f86fefd0eefa42319a1c183
Subham Mondal <subhammondal2502@gmail.com>
2026-09-14 09:23:50 +0530

    Add second number greater check .

--- Greater_Number_Checker.py
+++ Greater_Number_Checker.py
@@ -3,3 +3,5 @@ number1=int(input("enter 1st number :"))
 number2=int(input("enter 2nd number :"))
 if number1>number2:
     print("First number is greater")  
+elif number2>number1:
+    print("second number is greate")
