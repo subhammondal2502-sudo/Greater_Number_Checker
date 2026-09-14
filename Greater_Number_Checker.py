8b218b7e57b07d13adc0235a597e788826dbee8a
Subham Mondal <subhammondal2502@gmail.com>
2026-09-14 16:23:04 +0530

    Update Greater_Number_Checker.py

--- Greater_Number_Checker.py
+++ Greater_Number_Checker.py
@@ -4,7 +4,7 @@ number2=int(input("enter 2nd number :"))
 number3=int(input("enter 3rd number :"))
 if number1>number2 and number1>number3:
     print("First number is greater")  
-elif number2>number1:
+elif number2>number1 and number2>number3:
     print("second number is greate")
 else:
     print("Both numbers are equal")
